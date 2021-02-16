945534e3ef7a2738b4eae887333003e4178b1280
Michel França <fr.mich17@gmail.com>
2021-02-16 04:39:00 -0300

    Criando componentes

---
+++ src/components/Cliente.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="cliente">
+        <h2 id="cliente-nome">Cliente Aqui - Componente aqui</h2>
+        <h3>Descrição do cliente: </h3>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    #cliente {
+        color: green;
+        background-color: rgba(0, 0, 0, 0.1);
+        height: 120px;
+        max-width: 600px;
+    }
+
+    #cliente-nome {
+        color: #fff;
+    }
+</style>
